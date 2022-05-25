99ea2d07c356a4ef165b159fc6423f4a288678ef
worashf <uppert83@gmail.com>
2022-05-26 01:03:35 +0300

    update book list display

--- script.js
+++ script.js
@@ -1,63 +1,80 @@
+import Book from './Book.js';
 
- let listBook= document.getElementById("book-list");
- let addForm =document.getElementById("add-form");
- 
- class Book{
-    constructor(title,author){
-       this.title=title;
-       this.author=author
+const listBook = document.getElementById('book-list');
+const addForm = document.getElementById('add-form');
+
+class UI {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
     }
-    }
-
-
-class UI{
-
- static   getBook(){
-  let books;
-   (localStorage.getItem("books")===null)? books=[] : books=JSON.parse(localStorage.getItem("books"))
     return books;
-}
-   static storeBook(book){
-      let books = this.getBook();
-      books.push(book);
-      localStorage.setItem("books",JSON.stringify(books));
+  }
 
-    }
-   static displayBooks(){
+  static storeBook(book) {
+    const books = this.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 
+  static displayBooks() {
+    const books = UI.getBooks();
+    let bookDiv = '';
+
+    if (books.length !== 0 && books) {
+      books.forEach((book, index) => {
+        bookDiv += `
+    <div class="book-item ${index % 2 ? 'white-back' : 'gray-back'}">
+
+    <p class="book-info">${book.title} by ${book.author}</p>
+
+
+    <button type="button" id="remove-btn"  onClick='removeBook(${index})' class="remove-btn"> Remove </button>
+
+    </div>`;
+      });
+      listBook.innerHTML = bookDiv;
+      const removeBtn = document.querySelectorAll('.remove-btn');
+      removeBtn.forEach((button, index) => button.addEventListener('click', () => {
+        this.deleteBook(index);
+      }));
+    } else {
+      bookDiv += `
+            <div class="book">
+            <p class="book-message"> No book Available<p>
+            </div> `;
+      listBook.innerHTML = bookDiv;
     }
-static saveBook(title,author){
-    if(title && author){
-        let book = new Book(title,author);
-        this.storeBook(book);
+  }
+
+  static saveBook(title, author) {
+    if (title && author) {
+      const book = new Book(title, author);
+      this.storeBook(book);
+      this.displayBooks();
     }
-}
-static  deleteBook(){
-    let books = this.getBook();
+  }
+
+  static deleteBook(index) {
+    const books = this.getBooks();
     const filteredBook = books.filter((book, i) => index !== i);
     localStorage.setItem('books', JSON.stringify(filteredBook));
-
-    // display book
+    
+  }
 }
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let title = document.getElementById('title')
+  let author = document.getElementById('author')
+  if (title.value.trim !== '' && author.value.trim !== '') {
+    UI.saveBook(title.value, author.value);
+    title.value= '';
+    author.value = '';
+  }
+ 
+});
 
-
-}
-addForm.addEventListener("submit",e=>{
-    e.preventDefault();
-    let title =document.getElementById("title").value;
-    let author =document.getElementById("author").value;
-    if(title !== "" && author !==""){
-        UI.saveBook(title,author);
-    }
-})
-
-
-let screen = new UI();
-screen.getBook();
-
-
-
-
-
-
-
+UI.displayBooks();
